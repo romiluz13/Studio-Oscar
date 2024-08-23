abdb565b767963843a71d9418f8e4f96c2fa4575
romiluz13 <rom@iluz.net>
2024-08-23 14:14:06 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: 'localhost',
-    port: 5173,
-  },
-  base: '/Studio-Oscar-23.8/'
-})
+   export default defineConfig({
+     base: '/Studio-Oscar-23.8/',
+     build: {
+       outDir: 'dist', // or 'docs', depending on your GitHub Pages setup
+     },
+     // ... other config
+   });
